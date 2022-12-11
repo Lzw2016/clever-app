131dbc8afd9f5522ad8cfd1daa6822f7c546eded
lizw <1183409807@qq.com>
2022-12-11 21:58:39 +0800

    clever-data-redis

--- clever-data-redis/build.gradle.kts
+++ clever-data-redis/build.gradle.kts
@@ -1,2 +1,10 @@
 dependencies {
+    api("org.apache.commons:commons-pool2")
+    api("io.lettuce:lettuce-core")
+    api("org.springframework.boot:spring-boot-autoconfigure")
+    api("org.springframework.data:spring-data-redis")
+
+    // https://www.jianshu.com/p/fabcee952e04
+    api("org.redisson:redisson:3.18.1")
+    api("org.redisson:redisson-spring-boot-starter:3.18.1")
 }
