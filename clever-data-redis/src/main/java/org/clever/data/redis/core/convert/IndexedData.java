2c1c0a9db35f4ca0d21496ac52ccd4fe0217628d
lizw <1183409807@qq.com>
2023-02-12 11:41:26 +0800

    clever-data-redis

---
+++ clever-data-redis/src/main/java/org/clever/data/redis/core/convert/IndexedData.java
@@ -0,0 +1,21 @@
+//package org.clever.data.redis.core.convert;
+//
+///**
+// * {@link IndexedData} 表示给定键空间中属性路径的二级索引
+// * <p>
+// * 作者：lizw <br/>
+// * 创建时间：2023/02/01 15:28 <br/>
+// */
+//public interface IndexedData {
+//    /**
+//     * 获取索引名称的 {@link String} 表示
+//     *
+//     * @return 从不为 {@literal null}
+//     */
+//    String getIndexName();
+//
+//    /**
+//     * 获取索引所在的关联键空间
+//     */
+//    String getKeyspace();
+//}
