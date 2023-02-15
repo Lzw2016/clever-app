f0c59839ccc417e95fe4667f0cb4a1a08d4239eb
lizw <1183409807@qq.com>
2023-02-15 23:17:59 +0800

    clever-data-redis

--- clever-data-commons/src/main/java/org/clever/data/util/DirectFieldAccessFallbackBeanWrapper.java
+++ clever-data-commons/src/main/java/org/clever/data/util/DirectFieldAccessFallbackBeanWrapper.java
@@ -9,14 +9,12 @@ import java.lang.reflect.Field;
 import static org.clever.util.ReflectionUtils.*;
 
 /**
- * Custom extension of {@link BeanWrapperImpl} that falls back to direct field access in case the object or type being
- * wrapped does not use accessor methods.
+ * {@link BeanWrapperImpl} 的自定义扩展，在被包装的对象或类型不使用访问器方法的情况下回退到直接字段访问
  * <p>
  * 作者：lizw <br/>
  * 创建时间：2023/02/15 22:59 <br/>
  */
 public class DirectFieldAccessFallbackBeanWrapper extends BeanWrapperImpl {
-
     public DirectFieldAccessFallbackBeanWrapper(Object entity) {
         super(entity);
     }
@@ -25,48 +23,29 @@ public class DirectFieldAccessFallbackBeanWrapper extends BeanWrapperImpl {
         super(type);
     }
 
-    /*
-     * (non-Javadoc)
-     * @see org.springframework.beans.BeanWrapperImpl#getPropertyValue(java.lang.String)
-     */
     @Override
-
     public Object getPropertyValue(String propertyName) {
-
         try {
             return super.getPropertyValue(propertyName);
         } catch (NotReadablePropertyException e) {
-
             Field field = findField(getWrappedClass(), propertyName);
-
             if (field == null) {
-                throw new NotReadablePropertyException(getWrappedClass(), propertyName,
-                        "Could not find field for property during fallback access");
+                throw new NotReadablePropertyException(getWrappedClass(), propertyName, "Could not find field for property during fallback access");
             }
-
             makeAccessible(field);
             return getField(field, getWrappedInstance());
         }
     }
 
-    /*
-     * (non-Javadoc)
-     * @see org.springframework.beans.BeanWrapperImpl#setPropertyValue(java.lang.String, java.lang.Object)
-     */
     @Override
     public void setPropertyValue(String propertyName, Object value) {
-
         try {
             super.setPropertyValue(propertyName, value);
         } catch (NotWritablePropertyException e) {
-
             Field field = findField(getWrappedClass(), propertyName);
-
             if (field == null) {
-                throw new NotWritablePropertyException(getWrappedClass(), propertyName,
-                        "Could not find field for property during fallback access", e);
+                throw new NotWritablePropertyException(getWrappedClass(), propertyName, "Could not find field for property during fallback access", e);
             }
-
             makeAccessible(field);
             setField(field, getWrappedInstance(), value);
         }
