8ac36b602b587e18f0faf20970174983b01d6f09
lizw <1183409807@qq.com>
2023-02-19 15:50:21 +0800

    clever-data-redis

--- clever-core/src/main/java/org/clever/core/NamingUtils.java
+++ clever-core/src/main/java/org/clever/core/NamingUtils.java
@@ -2,7 +2,10 @@ package org.clever.core;
 
 import org.apache.commons.lang3.StringUtils;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -14,8 +17,12 @@ public class NamingUtils {
     public static final Pattern LOWER_CHAR_PATTERN = Pattern.compile("[a-z]+");
     public static final char UNDERLINE = '_';
 
+    // --------------------------------------------------------------------------------------------
+    // String
+    // --------------------------------------------------------------------------------------------
+
     /**
-     * 字符串驼峰转下划线格式(全小写)
+     * 驼峰 转 下划线(全小写)
      *
      * @param param 需要转换的字符串
      * @return 转换好的字符串
@@ -42,7 +49,7 @@ public class NamingUtils {
     }
 
     /**
-     * 字符串下划线转驼峰格式(小写驼峰)
+     * 下划线 转 驼峰(小写驼峰)
      *
      * @param param 需要转换的字符串
      * @return 转换好的字符串
@@ -68,89 +75,6 @@ public class NamingUtils {
             sb.append(c);
         }
         return sb.toString();
-    }
-
-    /**
-     * 下划线转驼峰格式映射缓存
-     *
-     * @param mapping 映射缓存
-     * @param dataMap 数据
-     */
-    private static void underlineToCamelMapping(Map<String, String> mapping, Map<String, Object> dataMap) {
-        if (mapping == null || dataMap == null) {
-            return;
-        }
-        for (String key : dataMap.keySet()) {
-            if (mapping.containsKey(key)) {
-                continue;
-            }
-            mapping.put(key, underlineToCamel(key));
-        }
-    }
-
-    /**
-     * key下划线转驼峰格式
-     *
-     * @param dataMap 数据
-     * @param mapping key重命名映射配置
-     */
-    public static Map<String, Object> mapKeyRename(Map<String, Object> dataMap, Map<String, String> mapping) {
-        if (dataMap == null) {
-            return null;
-        }
-        if (mapping == null || mapping.isEmpty()) {
-            mapping = new HashMap<>(dataMap.size());
-            underlineToCamelMapping(mapping, dataMap);
-        }
-        Map<String, Object> result = new LinkedHashMap<>(dataMap.size());
-        for (Map.Entry<String, Object> entry : dataMap.entrySet()) {
-            String key = mapping.getOrDefault(entry.getKey(), entry.getKey());
-            result.put(key, entry.getValue());
-        }
-        return result;
-    }
-
-    /**
-     * key下划线转驼峰格式
-     *
-     * @param dataMap 数据
-     */
-    public static Map<String, Object> underlineToCamel(Map<String, Object> dataMap) {
-        return mapKeyRename(dataMap, null);
-    }
-
-    /**
-     * map集合的key重命名
-     *
-     * @param dataMapList 数据集合
-     * @param mapping     key重命名映射配置
-     */
-    public static List<Map<String, Object>> mapKeyRename(List<? extends Map<String, Object>> dataMapList, Map<String, String> mapping) {
-        if (dataMapList == null) {
-            return null;
-        }
-        boolean mappingIsNull = false;
-        List<Map<String, Object>> result = new ArrayList<>(dataMapList.size());
-        for (Map<String, Object> map : dataMapList) {
-            if (mapping == null || mapping.isEmpty()) {
-                mapping = new HashMap<>(map.size());
-                mappingIsNull = true;
-            }
-            if (mappingIsNull) {
-                underlineToCamelMapping(mapping, map);
-            }
-            result.add(mapKeyRename(map, mapping));
-        }
-        return result;
-    }
-
-    /**
-     * key下划线转驼峰格式
-     *
-     * @param dataMapList 数据集合
-     */
-    public static List<Map<String, Object>> underlineToCamel(List<? extends Map<String, Object>> dataMapList) {
-        return mapKeyRename(dataMapList, null);
     }
 
     /**
@@ -182,5 +106,91 @@ public class NamingUtils {
                 break;
         }
         return name;
+    }
+
+    // --------------------------------------------------------------------------------------------
+    // Map
+    // --------------------------------------------------------------------------------------------
+
+    /**
+     * 下划线 转 驼峰 (映射缓存)
+     *
+     * @param dataMap        数据
+     * @param mapping        映射缓存(根据 dataMap 的 key 进行填充)
+     * @param renameStrategy key默认的重名了策略
+     */
+    private static void fillMapping(Map<String, Object> dataMap, Map<String, String> mapping, RenameStrategy renameStrategy) {
+        if (mapping == null || dataMap == null) {
+            return;
+        }
+        for (String key : dataMap.keySet()) {
+            if (mapping.containsKey(key)) {
+                continue;
+            }
+            mapping.put(key, rename(key, renameStrategy));
+        }
+    }
+
+    /**
+     * Map key 重命名
+     *
+     * @param dataMap        数据
+     * @param mapping        映射缓存(根据 dataMap 的 key 进行填充)
+     * @param renameStrategy key默认的重名了策略
+     * @return key 重命名后的 Map
+     */
+    public static Map<String, Object> renameMapKey(Map<String, Object> dataMap, Map<String, String> mapping, RenameStrategy renameStrategy) {
+        if (dataMap == null) {
+            return null;
+        }
+        if (mapping == null) {
+            mapping = new HashMap<>(dataMap.size());
+        }
+        fillMapping(dataMap, mapping, renameStrategy);
+        Map<String, Object> result = new LinkedHashMap<>(dataMap.size());
+        for (Map.Entry<String, Object> entry : dataMap.entrySet()) {
+            String key = entry.getKey();
+            key = mapping.getOrDefault(key, key);
+            result.put(key, entry.getValue());
+        }
+        return result;
+    }
+
+    /**
+     * Map key 下划线 转 驼峰(小写驼峰)
+     *
+     * @param dataMap 数据
+     * @param mapping key重命名映射配置
+     */
+    public static Map<String, Object> underlineToCamel(Map<String, Object> dataMap, Map<String, String> mapping) {
+        return renameMapKey(dataMap, mapping, RenameStrategy.ToCamel);
+    }
+
+    /**
+     * key下划线转驼峰格式
+     *
+     * @param dataMap 数据
+     */
+    public static Map<String, Object> underlineToCamel(Map<String, Object> dataMap) {
+        return renameMapKey(dataMap, null, RenameStrategy.ToCamel);
+    }
+
+    /**
+     * Map key 驼峰 转 下划线(全小写)
+     *
+     * @param dataMap 数据
+     * @param mapping key重命名映射配置
+     */
+    public static Map<String, Object> camelToUnderline(Map<String, Object> dataMap, Map<String, String> mapping) {
+        return renameMapKey(dataMap, mapping, RenameStrategy.ToUnderline);
+    }
+
+    /**
+     * Map key 驼峰 转 下划线(全小写)
+     *
+     * @param dataMap 数据
+     */
+    public static Map<String, Object> camelToUnderline(Map<String, Object> dataMap) {
+        return renameMapKey(dataMap, null, RenameStrategy.ToUnderline);
     }
 }
---
+++ clever-core/src/main/java/org/clever/core/SharedThreadPoolExecutor.java
@@ -0,0 +1,100 @@
+package org.clever.core;
+
+import org.apache.commons.lang3.concurrent.BasicThreadFactory;
+
+import java.util.concurrent.*;
+
+/**
+ * 全局共享的线程池
+ * <p>
+ * 作者：lizw <br/>
+ * 创建时间：2023/02/18 15:17 <br/>
+ */
+public class SharedThreadPoolExecutor {
+    private static ThreadPoolExecutor SMALL;
+    private static final Object LOCK_SMALL = new Object();
+    private static ThreadPoolExecutor NORMAL;
+    private static final Object LOCK_NORMAL = new Object();
+    private static ThreadPoolExecutor LARGE;
+    private static final Object LOCK_LARGE = new Object();
+
+    private static ThreadPoolExecutor createThreadPool(int core, int max, BlockingQueue<Runnable> queue, String namingPattern) {
+        ThreadPoolExecutor threadPool = new ThreadPoolExecutor(
+                core, max, 16, TimeUnit.SECONDS,
+                queue,
+                new BasicThreadFactory.Builder()
+                        .namingPattern(namingPattern)
+                        .daemon(true)
+                        .build(),
+                new ThreadPoolExecutor.CallerRunsPolicy()
+        );
+        threadPool.allowCoreThreadTimeOut(true);
+        return threadPool;
+    }
+
+    /**
+     * 较小的连接池 <br />
+     * <pre>{@code
+     * corePoolSize     = 2
+     * maximumPoolSize  = 32
+     * workQueue        = new SynchronousQueue<>()
+     * }</pre>
+     */
+    public static ThreadPoolExecutor getSmall() {
+        synchronized (LOCK_SMALL) {
+            if (SMALL == null) {
+                SMALL = createThreadPool(
+                        2,
+                        32,
+                        new SynchronousQueue<>(),
+                        "small-shared-%d"
+                );
+            }
+        }
+        return SMALL;
+    }
+
+    /**
+     * 正常大小的连接池 <br />
+     * <pre>{@code
+     * corePoolSize     = 16
+     * maximumPoolSize  = 32
+     * workQueue        = new ArrayBlockingQueue<>(64)
+     * }</pre>
+     */
+    public static ThreadPoolExecutor getNormal() {
+        synchronized (LOCK_NORMAL) {
+            if (NORMAL == null) {
+                NORMAL = createThreadPool(
+                        16,
+                        32,
+                        new ArrayBlockingQueue<>(64),
+                        "normal-shared-%d"
+                );
+            }
+        }
+        return NORMAL;
+    }
+
+    /**
+     * 较大的连接池 <br />
+     * <pre>{@code
+     * corePoolSize     = 512
+     * maximumPoolSize  = 1024
+     * workQueue        = new ArrayBlockingQueue<>(20480)
+     * }</pre>
+     */
+    public static ThreadPoolExecutor getLarge() {
+        synchronized (LOCK_LARGE) {
+            if (LARGE == null) {
+                LARGE = createThreadPool(
+                        512,
+                        1024,
+                        new ArrayBlockingQueue<>(20480),
+                        "large-shared-%d"
+                );
+            }
+        }
+        return LARGE;
+    }
+}
