ded44bd216d357ce3d815ba20ea3bf955aa38db4
lizw <1183409807@qq.com>
2023-02-20 17:50:17 +0800

    clever-security

--- clever-security/build.gradle.kts
+++ clever-security/build.gradle.kts
@@ -1,5 +1,7 @@
 dependencies {
     api(project(":clever-web"))
+    api(project(":clever-data-jdbc"))
+    api(project(":clever-data-redis"))
     api("io.jsonwebtoken:jjwt-api")
     api("io.jsonwebtoken:jjwt-impl")
     api("io.jsonwebtoken:jjwt-jackson")
