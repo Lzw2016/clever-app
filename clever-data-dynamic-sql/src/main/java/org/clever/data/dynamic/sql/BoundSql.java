1b5b65fa440c01d60e911b9b8978941569040ded
lizw <1183409807@qq.com>
2023-04-21 13:35:48 +0800

    修复sql参数名存在特殊字符问题

--- clever-data-dynamic-sql/src/main/java/org/clever/data/dynamic/sql/BoundSql.java
+++ clever-data-dynamic-sql/src/main/java/org/clever/data/dynamic/sql/BoundSql.java
@@ -13,15 +13,18 @@ public class BoundSql {
     @Getter
     private final Object parameterObject;
     /**
-     * 生成的SQL语句
+     * 生成的SQL语句(参数使用“?”占位)
      */
     @Getter
     private final String sql;
     /**
-     * 参数名称形式的sql
+     * 参数名称形式的sql(原始SQL,参数名中存在特殊字符: “.”)
      */
-    @Getter
-    private final String namedParameterSql;
+    private final String rawNamedParameterSql;
+    /**
+     * 参数名称形式的sql(参数名中无特殊字符)
+     */
+    private String namedParameterSql;
     /**
      * Sql参数名称列表(有顺序)
      */
@@ -49,9 +52,15 @@ public class BoundSql {
     @Getter
     private final Set<String> parameterExpressionSet = new LinkedHashSet<>();
 
+    /**
+     * @param sql               生成的SQL语句(参数使用“?”占位)
+     * @param namedParameterSql 参数名称形式的sql
+     * @param parameterList     Sql参数名称列表(有顺序)
+     * @param parameterObject   原始参数对象
+     */
     public BoundSql(String sql, String namedParameterSql, List<String> parameterList, Object parameterObject) {
         this.sql = sql;
-        this.namedParameterSql = namedParameterSql;
+        this.rawNamedParameterSql = namedParameterSql;
         this.parameterList = parameterList;
         this.parameterObject = parameterObject;
         this.additionalParameters = new HashMap<>();
@@ -80,15 +89,28 @@ public class BoundSql {
         return parameterMap;
     }
 
+    /**
+     * 参数名称形式的sql(参数名中无特殊字符)
+     */
+    public String getNamedParameterSql() {
+        if (namedParameterSql != null) {
+            return namedParameterSql;
+        }
+        initSqlParameter();
+        return namedParameterSql;
+    }
+
     private void initSqlParameter() {
         if (parameterList == null || parameterList.isEmpty()) {
             parameterValueList = Collections.emptyList();
             parameterMap = new HashMap<>();
+            namedParameterSql = rawNamedParameterSql;
             return;
         }
+        Map<String, String> renameParameter = new HashMap<>(parameterList.size());
         parameterValueList = new ArrayList<>(parameterList.size());
         parameterMap = new HashMap<>(parameterList.size());
-        parameterList.forEach(name -> {
+        for (final String name : parameterList) {
             Object value;
             if (hasAdditionalParameter(name)) {
                 value = getAdditionalParameter(name);
@@ -99,8 +121,28 @@ public class BoundSql {
                 value = metaObject.getValue(name);
             }
             parameterValueList.add(value);
-            parameterMap.put(name, value);
-        });
+            String newName = name;
+            if (needRename(name)) {
+                newName = renameParameter(name);
+                renameParameter.put(name, newName);
+            }
+            parameterMap.put(newName, value);
+        }
+        String namedSql = rawNamedParameterSql;
+        for (Map.Entry<String, String> entry : renameParameter.entrySet()) {
+            String name = ":" + entry.getKey();
+            String newName = ":" + entry.getValue();
+            namedSql = namedSql.replace(name, newName);
+        }
+        namedParameterSql = namedSql;
+    }
+
+    private boolean needRename(String parameterName) {
+        return parameterName != null && parameterName.contains(".");
+    }
+
+    private String renameParameter(String parameterName) {
+        return parameterName.replace('.', '$');
     }
 
     private boolean hasAdditionalParameter(String name) {
