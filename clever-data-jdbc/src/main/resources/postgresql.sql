bc52aaa0d05c622e74f4c4f937a242c9d3e3ac0d
lizw <1183409807@qq.com>
2023-12-05 10:54:25 +0800

    postgresql 存储过程 next_code

--- clever-data-jdbc/src/main/resources/postgresql.sql
+++ clever-data-jdbc/src/main/resources/postgresql.sql
@@ -81,3 +81,171 @@ create unique index sys_lock_lock_name on sys_lock (lock_name);
 
 --------------------------------------------------------------------------------------------------------------------------*/
 
+
+/* ====================================================================================================================
+    存储过程 procedure
+==================================================================================================================== */
+-- [存储过程]与Java语言相同的DateFormat规则的时间格式化函数
+create or replace function java_date_format(
+    date_time timestamp, pattern varchar
+)
+returns varchar
+language plpgsql
+as
+$function$
+DECLARE
+    _java_patterns  varchar[];
+    _pattern_map    json;
+    _key            varchar;
+    _value          varchar;
+    _pg_pattern     varchar;
+BEGIN
+    if(pattern is null or length(trim(pattern))<=0) then
+        return '';
+    end if;
+    if(date_time is null) then
+        return '';
+    end if;
+    /*
+    DateFormat 规则
+    ----------------------------------------
+    Postgresql  Java    描述
+    ----------------------------------------
+    DDD         D       年份中的天数
+    MS          SSS     毫秒数
+    YYYY        yyyy    4位年份
+    YY          yy      2位年份
+    MM          MM      月份
+    DD          dd      月份中的天数
+    HH24        HH      24小时数
+    HH12        hh      12小时数
+    MI          mm      分钟数
+    SS          ss      秒数
+    */
+    _java_patterns := array['D', 'SSS', 'yyyy', 'yy', 'MM', 'dd', 'HH', 'hh', 'mm', 'ss'];
+    _pattern_map := '{"yy": "YY", "yyyy": "YYYY", "MM": "MM", "dd": "DD", "HH": "HH24", "hh": "HH12", "mm": "MI", "ss": "SS", "SSS": "MS", "D": "DDD"}'::json;
+    -- 确保 pattern 中只包含支持的 DateFormat 规则
+    -- _pg_pattern := pattern;
+    -- for _key in select json_object_keys(_pattern_map) loop
+    --     _pg_pattern := replace(_pg_pattern, _key, '');
+    -- end loop;
+    -- if(length(trim(_pg_pattern))>0) then
+    --     raise exception using message = -20000, hint = ('不支持的 pattern=[' || _pg_pattern || ']');
+    -- end if;
+    -- 替换 DateFormat 规则
+    _pg_pattern := pattern;
+    foreach _key in array _java_patterns loop
+        _value := _pattern_map->>_key;
+        _pg_pattern := replace(_pg_pattern, _key, _value);
+    end loop;
+    -- call p_log('java_date_format', '_pg_pattern=' || _pg_pattern);
+    return to_char(date_time, _pg_pattern);
+END;
+$function$
+;
+
+-- [存储过程]批量获取业务code值
+create or replace function next_code(
+    codename varchar, size int4
+)
+returns varchar[]
+language plpgsql
+as
+$function$
+DECLARE
+    _row                record;
+    _new_reset_flag     varchar;
+    _new_sequence       bigint;
+    _new_pattern        varchar;
+    _matches            varchar[];
+    _match              varchar;
+    _match_pattern      varchar;
+    _match_value        varchar;
+    _seq_digit_str      varchar;
+    _seq_digit          int4;
+    _seq_placeholder    varchar = '@@@_seq_@@@';
+    _res_seq            varchar;
+    _res                varchar[];
+BEGIN
+    if (size is null or size <= 0) then
+        raise exception using message = -20000, hint = ('参数 size=[' || codename || '] 错误');
+    end if;
+    select
+        id, pattern, sequence, reset_pattern, reset_flag, now() at time zone 'Asia/Shanghai' as now into _row
+    from biz_code where code_name = codename for update;
+    if (_row is null) then
+        raise exception using message = -20000, hint = ('biz_code 表数据 code_name=[' || codename || ']不存在');
+    end if;
+    -- 计算 reset_flag
+    _new_reset_flag := _row.reset_flag;
+    _new_sequence := _row.sequence;
+    if (_row.reset_pattern is not null and length(trim(_row.reset_pattern)) > 0) then
+        _new_reset_flag := java_date_format(_row.now, _row.reset_pattern);
+    end if;
+    -- 判断是否需要重置 sequence 计数
+    if (_new_reset_flag is distinct from _row.reset_flag) then
+        _new_sequence := 0;
+    end if;
+    _new_sequence := _new_sequence + size;
+    -- 更新数据库值
+    update biz_code set sequence=_new_sequence, reset_flag=_new_reset_flag, update_at=now() where id = _row.id;
+    -- 处理 pattern
+    _new_pattern := _row.pattern;
+    _res := array[size]; -- ::varchar[];
+    for _matches in
+        select regexp_matches(_row.pattern, '\$\{.*?}', 'g')
+        loop
+            foreach _match in array _matches loop
+                _match_pattern := substring(_match, 3, length(_match) - 3);
+                -- call p_log('next_code', 'match_pattern=' || _match_pattern);
+                if (_match_pattern like ('seq%')) then
+                    _seq_digit_str = substring(_match_pattern, 4);
+                    if (_seq_digit_str is not null and length(_seq_digit_str) > 0) then
+                        _seq_digit := _seq_digit_str::int4;
+                    end if;
+                    _new_pattern := replace(_new_pattern, _match, _seq_placeholder);
+                elsif (_match_pattern like ('id%')) then
+                    _seq_digit_str = substring(_match_pattern, 3);
+                    if (_seq_digit_str is not null and length(_seq_digit_str) > 0) then
+                        _seq_digit := _seq_digit_str::int4;
+                    end if;
+                    _new_pattern := replace(_new_pattern, _match, _seq_placeholder);
+                else
+                    _match_value := java_date_format(_row.now, _match_pattern);
+                    _new_pattern := replace(_new_pattern, _match, _match_value);
+                end if;
+            end loop;
+        end loop;
+    -- 生成返回值
+    _new_sequence := _new_sequence - size;
+    for i in 1..size loop
+        if(_seq_digit is not null) then
+            _res_seq := (_new_sequence + i)::varchar;
+            if (length(_res_seq) > _seq_digit) then
+                _res_seq := substring(_res_seq, length(_res_seq) + 1 - _seq_digit);
+            else
+                _res_seq := lpad(_res_seq, _seq_digit, '0');
+            end if;
+            _res[i] := replace(_new_pattern, _seq_placeholder, _res_seq);
+        else
+            _res[i] := _new_pattern;
+        end if;
+    end loop;
+    return _res;
+END;
+$function$
+;
+
+-- [存储过程]获取下一个业务code值
+create or replace function next_code(
+    codename varchar
+)
+returns varchar
+language plpgsql
+as
+$function$
+BEGIN
+    return (next_code(codename, 1)::varchar[])[1];
+END;
+$function$
+;
