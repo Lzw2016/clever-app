1f2a716088f7dd8c28d89f083fe47eb599d4826a
lizw <1183409807@qq.com>
2024-01-28 11:23:40 +0800

    clever-task 对于 java、js 任务需要支持 JobContext

--- clever-boot/src/main/java/org/clever/app/task/TaskTest.java
+++ clever-boot/src/main/java/org/clever/app/task/TaskTest.java
@@ -2,6 +2,7 @@ package org.clever.app.task;
 
 import lombok.extern.slf4j.Slf4j;
 import org.clever.core.DateUtils;
+import org.clever.task.core.job.JobContext;
 
 /**
  * 作者：lizw <br/>
@@ -11,5 +12,9 @@ import org.clever.core.DateUtils;
 public class TaskTest {
     public static void job01() {
         log.info("定时任务: {}", DateUtils.getCurrentDate("yyyy-MM-dd HH:mm:ss.SSS"));
+    }
+
+    public static void job02(JobContext context) {
+        log.info("定时任务: dbNow={}", context.getDbNow());
     }
 }
