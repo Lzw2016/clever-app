5e21fba36c53c5aa9cceeaed770d4fa8da83e4b7
lizw <1183409807@qq.com>
2024-09-20 23:32:29 +0800

    升级到 springboot3.x

--- clever-core/src/test/java/org/clever/core/validator/BaseValidatorUtilsTest.java
+++ clever-core/src/test/java/org/clever/core/validator/BaseValidatorUtilsTest.java
@@ -4,10 +4,10 @@ import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
-import javax.validation.Valid;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotBlank;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import java.util.List;
 
 /**
